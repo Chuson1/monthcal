68198f0aa4695a54bd9fb231c858978b787e3ecc
Koki Nakamura <a77k21mn@yahoo.co.jp>
2020-01-04 17:34:33 +0000

    Userモデルvalidates,flashメッセージ表示の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+	validates :name, presence: true,
+			length: { maximum: 16 }
+	
+	# @記号必須の正規表現
+	VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
+	validates :email, presence: true,
+			format: { with: VALID_EMAIL_REGEX }
 end
