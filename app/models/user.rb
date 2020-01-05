c982b9f2cfb0fa28df0fd5a661b485b386ee0412
Koki Nakamura <a77k21mn@yahoo.co.jp>
2020-01-05 07:38:55 +0000

    user.rb,passwordカラムバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
 			format: { with: VALID_EMAIL_REGEX }
 	
 	has_secure_password
+	
+	# パスワード文字数8〜32。アルファベット、数字の混合以外の値を受け付けない
+	VALID_PASSWORD_REGEX = /\A(?=\d{0,99}+[a-z])(?=[a-z]{0,99}+\d)[a-z\d]{8,32}+\z/i
+	validates :password, presence: true,
+			format: { with: VALID_PASSWORD_REGEX }
 end
