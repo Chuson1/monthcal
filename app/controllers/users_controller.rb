7df01914317450b52761b0101b93c65ee2672f63
Koki Nakamura <a77k21mn@yahoo.co.jp>
2020-01-07 12:08:13 +0000

    showページ追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only:[:edit, :update, :destroy]
   
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+  
   def new
     @user = User.new
   end
